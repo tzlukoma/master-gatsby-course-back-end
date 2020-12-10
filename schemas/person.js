1e21df16b19e488761835d6caabafe0c5c9d6fc0
Thomas Z Lukoma <tzlukoma@gmail.com>
2020-12-10 16:50:49 -0500

    feat: :sparkles: Adds Person data type

---
+++ schemas/person.js
@@ -0,0 +1,41 @@
+import { MdPerson as icon } from 'react-icons/md';
+
+export default {
+  name: 'person',
+  title: 'Slicemasters',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      options: {
+        source: 'Tell us a bit about them',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
